1741739e756863c5b89abd106104f0d16b9a83e3
bcohn12 <bcohn12@gmail.com>
2016-03-15 09:44:00 +0100

    converted hasher to proper DOI generator

--- task_4_hashing/hasher.py
+++ task_4_hashing/hasher.py
@@ -8,6 +8,11 @@ def id_number(your_string):
   my_id = int(hashlib.md5(encoded_str).hexdigest(), 16)
   return(my_id)
 
+def compose_doi(your_id_number, prefix="10.1234", final_suffix="-polar-enrich"):
+  return(prefix + "/" + your_id_number + final_suffix)
+
+def filename_to_doi(filename_string):
+  return(compose_doi(str(id_number(filename_string))))
 ##################################
 #Testing Framework
 ##################################
@@ -17,12 +22,16 @@ import unittest
  
 class TestUM(unittest.TestCase):
  
-	def setUp(self):
-		pass
+  def setUp(self):
+    pass
 
-	def test_hex_hash_examples(self):
-		self.assertEqual(id_number("test_img_files/IMG_1305.jpg"), 279558192375589665600342317696327885246)
+  def test_hex_hash_examples(self):
+    self.assertEqual(id_number("test_img_files/IMG_1305.jpg"), 279558192375589665600342317696327885246)
+    
+  def test_enrichment_doi(self):
+    self.assertEqual(filename_to_doi("test_img_files/IMG_1305.jpg"),"10.1234/279558192375589665600342317696327885246-polar-enrich")
+
 
 if __name__ == '__main__':
-	unittest.main()
-	print('Unit tests complete')
+  unittest.main()
+  print('Unit tests complete')
