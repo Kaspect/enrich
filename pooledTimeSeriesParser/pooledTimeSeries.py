11281067423e6888c8a908cb2f94e30a54f7bde9
dhruvbha <dhruvbha@usc.edu>
2016-04-04 18:52:41 -0700

    Running PooledTimeSeries

--- pooledTimeSeriesParser/pooledTimeSeries.py
+++ pooledTimeSeriesParser/pooledTimeSeries.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import json
 
 def runProcess(exe):
    p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -11,13 +12,20 @@ def runProcess(exe):
        break
 
 def get_filepaths(directory):
+    finalList = []
     for root, directories, files in os.walk(directory):
         for filename in files:
             filepath = os.path.join(root, filename)
             print filepath
             command = 'java -classpath /Users/dhruvbhatia/Desktop/pooledTimeSeries/tika-app-1.12.jar org.apache.tika.cli.TikaCLI --config=/Users/dhruvbhatia/Desktop/pooledTimeSeries/tika-config.xml -J ' + filepath
-            f = open('/Users/dhruvbhatia/Desktop/output/output_' + filename + '.json','w')
+            list = []
             for line in runProcess(command.split()):
-                f.write(line)
-            f.close()
+                list.append(line)
+            finalList.append(list)
+    f = open('/Users/dhruvbhatia/Desktop/output.json','w')
+    data = json.dumps(finalList)
+    
+    f.write(data)
+    f.close()
+
 get_filepaths("/Users/dhruvbhatia/Desktop/pooledTimeSeries/mp4/")
